a0874742ad1668ef42afee6765f1f578f71f9fa4
mdMandoinfo <mdMandoinfo@gmail.com>
2022-05-06 21:04:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-// Get User IP & Get visitor IP
-function getVisitorIP()
+// Get visitor IP
+function GetVisitorIP()
 {
     // Get real visitor IP behind CloudFlare network
     if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
@@ -11,24 +11,30 @@ function getVisitorIP()
     $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
     $remote  = $_SERVER['REMOTE_ADDR'];
 
-    if(filter_var($client, FILTER_VALIDATE_IP))
-    {
+    if(filter_var($client, FILTER_VALIDATE_IP)){
         $ip = $client;
-    }
-    elseif(filter_var($forward, FILTER_VALIDATE_IP))
-    {
+    }elseif(filter_var($forward, FILTER_VALIDATE_IP)){
         $ip = $forward;
-    }
-    else
-    {
+    }else {
         $ip = $remote;
     }
-
     return $ip;
 }
 
-
-$VisitorIP = getVisitorIP();
-echo $VisitorIP . "<br>"; // Print User IP
+$VisitorIP = GetVisitorIP();
 /*!!! Copyright (c) 2022 mdMandoinfo !!!*/
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Get real visitorIP behind CloudFlare network</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">
+</head>
+<body>
+
+<h1></h1>
+<p><?php echo "<center><b>Your  Ip is</b>: ". $VisitorIP. "</center>"; ?></p>
+</body>
+</html>

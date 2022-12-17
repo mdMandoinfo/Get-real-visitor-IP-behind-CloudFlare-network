001300f48e0776dacbcfd2f16ee1c6950746f3e6
mdMandoinfo <mdMandoinfo@gmail.com>
2022-12-17 20:39:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,34 @@
 <?php
 // Get visitor IP
-function GetVisitorIP()
-{
+function GetVisitorIP() {
     // Get real visitor IP behind CloudFlare network
     if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
         $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
         $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
     }
+
     $client  = @$_SERVER['HTTP_CLIENT_IP'];
     $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
     $remote  = $_SERVER['REMOTE_ADDR'];
 
-    if(filter_var($client, FILTER_VALIDATE_IP)){
+    if (filter_var($client, FILTER_VALIDATE_IP)) {
+
         $ip = $client;
-    }elseif(filter_var($forward, FILTER_VALIDATE_IP)){
+
+    } elseif (filter_var($forward, FILTER_VALIDATE_IP)) {
+
         $ip = $forward;
-    }else {
+
+    } else {
+
         $ip = $remote;
+
     }
+
     return $ip;
 }
 
-$VisitorIP = GetVisitorIP();
+$VisitorIP = GetVisitorIP(); 
     echo $VisitorIP; // Print Visitor IP
+
 ?>
